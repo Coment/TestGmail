8815f2204f0f7a836da8f53a8ccc4a107a6cff76
Coment <charcoment@gmail.com>
2015-05-07 14:14:09 +0300

    Update TestBase.java

--- src/test/java/com/gmail/testbase/TestBase.java
+++ src/test/java/com/gmail/testbase/TestBase.java
@@ -8,7 +8,6 @@ import org.openqa.selenium.support.events.EventFiringWebDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Parameters;
-
 import com.gmail.allpages.AccountPage;
 import com.gmail.allpages.HomePage;
 import com.gmail.util.Browser;
@@ -16,7 +15,7 @@ import com.gmail.util.PropertyLoader;
 import com.gmail.webdriver.WebDriverFactory;
 
 
-/*
+/**
  * Base class for all the test classes
  * 
  * @author Yuri Demchenko
