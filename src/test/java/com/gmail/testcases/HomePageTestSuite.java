600274d9bb12a4f475a89da82051a66d72dec43c
Coment <charcoment@gmail.com>
2015-05-07 14:15:59 +0300

    Update HomePageTestSuite.java

--- src/test/java/com/gmail/testcases/HomePageTestSuite.java
+++ src/test/java/com/gmail/testcases/HomePageTestSuite.java
@@ -9,7 +9,7 @@ public class HomePageTestSuite extends TestBase{
 	@Test (groups = {"group"})
 	//TS 1.1
 	public void openHomePage(){
-		Assert.assertTrue(home.isSiteLogoDisplayed());
+		Assert.assertTrue(home.isSiteLogoDisplayed());  
 	}
 	
 	
